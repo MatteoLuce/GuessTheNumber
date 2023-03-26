4fd79eb7b8ed8771c896998c4b08bb93d1bc3b12
Matteo Lucerni <zzzmat01.luce@gmail.com>
2023-03-26 18:14:06 +0200

    giorno 26 ore 18:14

--- GuessTheNumber/script.js
+++ GuessTheNumber/script.js
@@ -11,20 +11,41 @@ document.getElementById("submit").onclick = function verifyNumber (){
     
     if (userGuess == myNumber) {
         result = `${userGuess}?  You get it! Good job mate`;
-        document.getElementById("result").style.color = "green"
+        document.getElementById("result").style.color = "green";
+        countDisplay.innerHTML = `You guessed it in ${count} attempts`;
+        localStorage.setItem("lastscore", count);
     } else if (userGuess < myNumber) {
         result = `${userGuess}?  The number is too small!` 
         document.getElementById("result").style.color = "orange"
-    } else if (userGuess > myNumber) {
+    } else {
         result = `${userGuess}?  The number is too high!`
         document.getElementById("result").style.color = "red"
     } 
 
-    document.getElementById("result").innerText = result
+    document.getElementById("result").innerText = result;
+
+    
 }
 
 
+var input = document.getElementById("guess");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("submit").click();
+    }
+});
+
+var count = 1;
+      var button = document.getElementById("submit");
+      var countDisplay = document.getElementById("count");
+      button.addEventListener("click", function() {
+         count++;
+      });
+
 document.getElementById("reload").onclick = refreshPage;
+
+
 
 function refreshPage (){
     
@@ -32,4 +53,6 @@ function refreshPage (){
     setTimeout(()=> { window.location.reload(); },2000);
     
 }
+
+document.getElementById("demo").innerHTML = `Your last score was: ${localStorage.getItem("lastscore")} attemps`;
 
